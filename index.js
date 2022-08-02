2ff53c702d37bbed2753cb2637adf5e3644b5728
Juan Diego Gimenez Pena <juandiego.gimenez@globant.com>
2022-08-02 17:36:33 -0500

    Creating coming soon page

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ const app = express();
 const path = require('path');
 
 app.get('/', (req, res) => {
-	res.sendFile(path.join(__dirname + '/client/index.html'));
+	res.sendFile(path.join(__dirname + '/index.html'));
 });
+
+app.use(express.static(__dirname + '/public'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
